c4c4eeffc63f1ff93d8f649a2d7850bd32588ad0
Flucus ~ NightSky <114465882+Flucus@users.noreply.github.com>
2023-02-17 12:09:21 +0800

    Add files via upload

---
+++ Extension.js
@@ -0,0 +1,1 @@
+javascript:location.href='https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion=%27+(navigator.appVersion.match(/Chrome\/(\S+)/)[1])+%27&x=id%%27+%273D%27+(document.querySelector(%27a[href^="https://chrome.google.com/webstore/report/"]%27).pathname.match(/[^\/]+\/*$/)[0])+%27%%27+%2726installsource%%27+%273Dondemand%%27+%2726uc%27;
